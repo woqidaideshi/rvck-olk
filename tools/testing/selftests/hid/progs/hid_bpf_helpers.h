7c4ec1248135ad41117550221cf7adee06dcd8d3
Benjamin Tissoires <bentiss@kernel.org>
2023-09-08 22:22:38 +0000

    selftests/hid: ensure we can compile the tests on kernels pre-6.3
    For the hid-bpf tests to compile, we need to have the definition of
    struct hid_bpf_ctx. This definition is an internal one from the kernel
    and it is supposed to be defined in the generated vmlinux.h.
    
    This vmlinux.h header is generated based on the currently running kernel
    or if the kernel was already compiled in the tree. If you just compile
    the selftests without compiling the kernel beforehand and you are running
    on a 6.2 kernel, you'll end up with a vmlinux.h without the hid_bpf_ctx
    definition.
    
    Use the clever trick from tools/testing/selftests/bpf/progs/bpf_iter.h
    to force the definition of that symbol in case we don't find it in the
    BTF and also add __attribute__((preserve_access_index)) to further
    support CO-RE functionality for these tests.
    
    Signed-off-by: Justin Stitt <justinstitt@google.com>
    Signed-off-by: Benjamin Tissoires <bentiss@kernel.org>

--- tools/testing/selftests/hid/progs/hid_bpf_helpers.h
+++ tools/testing/selftests/hid/progs/hid_bpf_helpers.h
@@ -5,6 +5,55 @@
 #ifndef __HID_BPF_HELPERS_H
 #define __HID_BPF_HELPERS_H
 
+/* "undefine" structs in vmlinux.h, because we "override" them below */
+#define hid_bpf_ctx hid_bpf_ctx___not_used
+#define hid_report_type hid_report_type___not_used
+#define hid_class_request hid_class_request___not_used
+#define hid_bpf_attach_flags hid_bpf_attach_flags___not_used
+#include "vmlinux.h"
+#undef hid_bpf_ctx
+#undef hid_report_type
+#undef hid_class_request
+#undef hid_bpf_attach_flags
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <linux/const.h>
+
+enum hid_report_type {
+	HID_INPUT_REPORT		= 0,
+	HID_OUTPUT_REPORT		= 1,
+	HID_FEATURE_REPORT		= 2,
+
+	HID_REPORT_TYPES,
+};
+
+struct hid_bpf_ctx {
+	__u32 index;
+	const struct hid_device *hid;
+	__u32 allocated_size;
+	enum hid_report_type report_type;
+	union {
+		__s32 retval;
+		__s32 size;
+	};
+} __attribute__((preserve_access_index));
+
+enum hid_class_request {
+	HID_REQ_GET_REPORT		= 0x01,
+	HID_REQ_GET_IDLE		= 0x02,
+	HID_REQ_GET_PROTOCOL		= 0x03,
+	HID_REQ_SET_REPORT		= 0x09,
+	HID_REQ_SET_IDLE		= 0x0A,
+	HID_REQ_SET_PROTOCOL		= 0x0B,
+};
+
+enum hid_bpf_attach_flags {
+	HID_BPF_FLAG_NONE = 0,
+	HID_BPF_FLAG_INSERT_HEAD = _BITUL(0),
+	HID_BPF_FLAG_MAX,
+};
+
 /* following are kfuncs exported by HID for HID-BPF */
 extern __u8 *hid_bpf_get_data(struct hid_bpf_ctx *ctx,
 			      unsigned int offset,
