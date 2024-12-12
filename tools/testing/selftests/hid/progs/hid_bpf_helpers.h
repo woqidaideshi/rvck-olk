5f9168ca56cd7c0f12d70abb8cb1545308ff6b20
Yafen Fang <yafen@iscas.ac.cn>
2024-12-12 16:58:33 +0800

    fix: redefinition of hid_report_type and hid_class_request

--- tools/testing/selftests/hid/progs/hid_bpf_helpers.h
+++ tools/testing/selftests/hid/progs/hid_bpf_helpers.h
@@ -7,26 +7,14 @@
 
 /* "undefine" structs in vmlinux.h, because we "override" them below */
 #define hid_bpf_ctx hid_bpf_ctx___not_used
-#define hid_report_type hid_report_type___not_used
-#define hid_class_request hid_class_request___not_used
 #define hid_bpf_attach_flags hid_bpf_attach_flags___not_used
 #include "vmlinux.h"
 #undef hid_bpf_ctx
-#undef hid_report_type
-#undef hid_class_request
 #undef hid_bpf_attach_flags
 
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <linux/const.h>
-
-enum hid_report_type {
-	HID_INPUT_REPORT		= 0,
-	HID_OUTPUT_REPORT		= 1,
-	HID_FEATURE_REPORT		= 2,
-
-	HID_REPORT_TYPES,
-};
 
 struct hid_bpf_ctx {
 	__u32 index;
@@ -38,15 +26,6 @@ struct hid_bpf_ctx {
 		__s32 size;
 	};
 } __attribute__((preserve_access_index));
-
-enum hid_class_request {
-	HID_REQ_GET_REPORT		= 0x01,
-	HID_REQ_GET_IDLE		= 0x02,
-	HID_REQ_GET_PROTOCOL		= 0x03,
-	HID_REQ_SET_REPORT		= 0x09,
-	HID_REQ_SET_IDLE		= 0x0A,
-	HID_REQ_SET_PROTOCOL		= 0x0B,
-};
 
 enum hid_bpf_attach_flags {
 	HID_BPF_FLAG_NONE = 0,
